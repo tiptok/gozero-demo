e8fa515adab4841155a44bd9c4d428d494af107a
yangfu <785409885@qq.com>
2022-10-11 12:00:43 +0800

    refactor: grpc error

--- app/usercenter/cmd/rpc/internal/logic/getuserinfologic.go
+++ app/usercenter/cmd/rpc/internal/logic/getuserinfologic.go
@@ -2,7 +2,10 @@ package logic
 
 import (
 	"context"
+	"fmt"
 	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"zero-demo/app/usercenter/cmd/model"
 	"zero-demo/app/usercenter/cmd/rpc/usercenter"
 	"zero-demo/app/usercenter/internal/pkg/db/transaction"
@@ -62,7 +65,24 @@ func (l *GetUserInfoLogic) GetUserInfo(in *pb.GetUserInfoReq) (*pb.GetUserInfoRe
 		_ = copier.Copy(&respUser, user)
 		return nil
 	}, false); err != nil {
-		return nil, err
+		// example: 替换 gRPC error
+		st := status.New(codes.DeadlineExceeded, "some error")
+		//st := status.Error(codes.DeadlineExceeded, "some error")
+		/*
+		  header grpc-status-details-bin，用來補足 status 表現能力不夠的問題。為了統一模型，這個資訊格式也是採用 protobuf，我們可以把它想像成 error 專用欄位，內容經過 protobuf message 編碼後，會放在這個標頭中
+		*/
+		st, _ = st.WithDetails(&usercenter.GetUserInfoResp{})
+		// 解析错误
+		stFrom, _ := status.FromError(st.Err())
+		if st.Code() == codes.DeadlineExceeded {
+			for _, d := range stFrom.Details() {
+				switch info := d.(type) {
+				case *usercenter.GetUserInfoResp:
+					fmt.Println(info)
+				}
+			}
+		}
+		return nil, st.Err()
 	}
 	return &pb.GetUserInfoResp{
 		User: &respUser,
